fb7e76fca3f8a6a5a068c7930161c116980a5723
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2022-09-18 16:51:16 +0530

    Create c.java

---
+++ Journal/pract1/c.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class c{
+    public static void main(String... args){
+        byte myByte = 100;
+        int myInt = myByte;
+        long myLong = myInt;
+        float myFloat = myLong;
+        double myDouble = myFloat;
+        System.out.println("byte   = "+myByte+"\nint    = "+myInt+"\nlong   = "+myLong+"\nfloat  = "+myFloat+"\ndouble = "+myDouble);
+    }
+}
