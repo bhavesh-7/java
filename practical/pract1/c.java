05122f020efd1e588e57cda87e6dfa8d740aac97
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2022-10-27 21:31:04 +0530

    Create c.java

---
+++ practical/pract1/c.java
@@ -0,0 +1,16 @@
+class c{
+    public static void main(String... args){
+        byte a=10;
+        short b = a;
+        int c = b;
+        long d = c;
+        float e = d;
+        double f = e;
+        System.out.println("Byte = "+a);
+        System.out.println("Typecasting Short = "+b);
+        System.out.println("Typecasting int = "+c);
+        System.out.println("Typecasting long = "+d);
+        System.out.println("Typecasting float = "+e);
+        System.out.println("Typecasting double = "+f);
+    }
+}
