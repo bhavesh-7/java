8a47929f2e4443576feea7716c50d45f8a4eed8a
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2022-10-27 20:53:05 +0530

    Create b.java

---
+++ practical/pract1/b.java
@@ -0,0 +1,10 @@
+class b{
+    public static void main(String... args){
+        //let
+        int h,m,calc;
+        h=4;
+        calc=h*60;
+        m=calc;
+        System.out.println(h+" Hours = "+m+" Minutes");
+    }
+}
