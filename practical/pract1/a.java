428164f09e03c035f05e110db5bf026860713eee
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2022-10-27 20:52:32 +0530

    Create a.java

---
+++ practical/pract1/a.java
@@ -0,0 +1,11 @@
+class a{
+  public static void main(String... args){
+    float pi =3.14;
+    float area,perimeter,raduis;
+    radius=6;
+    area=pi*radius*radius;
+    perimeter=2*pi*radius;
+    System.out.println("Area of circle is = "+area);
+    System.out.println("Perimeter of circle is = "+perimerter);
+  }
+}
